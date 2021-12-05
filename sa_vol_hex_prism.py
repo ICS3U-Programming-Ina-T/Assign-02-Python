cef74a2acf254dc041a00995c16e8611707b2bfe
Ubuntu <ubuntu@ip-172-31-5-22.ec2.internal>
2021-12-05 18:49:16 +0000

    Created the sa_vol_hex_prism.py file. This program intakes user inpur to calculate and display the surface area & volume of a hexagonal prism. Text is coloured and units are asked for from the user

---
+++ sa_vol_hex_prism.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# Created by: Ina Tolo
+# Created on: Nov. 30, 2021
+# This program asks the user for the base edge and
+# height of the hexagonal prism in their chosen units. It then
+# calculates and displays the surface area and
+# volume.
+import math
+import colorama
+from colorama import Fore
+
+
+def main():
+    # input
+
+    print("Today we will calculate the surface area and")
+    print("volume of a hexagonal prism!")
+    print("")
+    units = str(input("Enter the units: "))
+    base_edge = int(input("Enter the base edge: "))
+    height = int(input("Enter the height: "))
+
+    # calculation process for surface area
+
+    surface_area = (3 * math.sqrt(3))
+    surface_area1 = pow(base_edge, 2)
+    surface_area2 = surface_area * surface_area1
+    surface_area3 = 6 * base_edge
+    surface_area4 = surface_area3 * height
+    sA5 = surface_area2 + surface_area4
+
+    # calculation process for volume
+
+    volume = (3 * math.sqrt(3)) / 2
+    volume1 = pow(base_edge, 2)
+    volume2 = volume1 * height
+    volume3 = volume * volume2
+
+    # output
+    print("")
+    print(Fore.RED + "Surface area = {:,.2f}{}^2 ". format(sA5, units))
+    print(Fore.GREEN + "Volume = {:,.2f}{}^3". format(volume3, units))
+
+
+if __name__ == "__main__":
+    main()
